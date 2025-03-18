2691b8732e546eb955afc0c0cfb0597d2726726e
Yorly <yquinteropa@ufpso.edu.co>
2025-03-18 07:01:21 -0500

    Primer commit se crea el repositorio compartido

---
+++ src/main/java/com/mycompany/parcialestructura/ParcialEstructura.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.parcialestructura;
+
+/**
+ *
+ * @author SCIS2-27
+ */
+public class ParcialEstructura {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
